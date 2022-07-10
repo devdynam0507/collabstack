8eb5f583cf3f7af4af845f625e924b3e99eeab61
devdynam0507 <wsnam0507@naver.com>
2022-07-10 13:31:38 +0900

    collabstack be initial commit

---
+++ collabstack-be/common/src/main/java/net/collabstack/common/CollabStackApp.java
@@ -0,0 +1,13 @@
+package net.collabstack.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabStackApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabStackApp.class, args);
+    }
+
+}
