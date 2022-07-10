f78859c8708161a8293fa850faf76e1ec7dcf8a1
devdynam0507 <wsnam0507@naver.com>
2022-07-10 21:50:18 +0900

    실패하는 테스트 추가 (깃헙 액션 테스트용)

---
+++ collabstack-be/common/src/test/java/net/collabstack/common/CollabTests.java
@@ -0,0 +1,14 @@
+package net.collabstack.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CollabTests {
+
+    @Test
+    public void failed() {
+        assertThat(true).isEqualTo(false);
+    }
+
+}
