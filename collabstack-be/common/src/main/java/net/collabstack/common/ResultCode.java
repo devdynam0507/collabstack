844e0f31247abd9a983e2233de34569d5300bede
devdynam0507 <wsnam0507@naver.com>
2022-07-17 00:43:14 +0900

    공통 응답 DTO 추가

---
+++ collabstack-be/common/src/main/java/net/collabstack/common/ResultCode.java
@@ -0,0 +1,14 @@
+package net.collabstack.common;
+
+public enum ResultCode {
+
+    OK,
+    UNAUTHORIZED,
+    FORBIDDEN,
+    BAD_REQUEST,
+    NOT_FOUND,
+    INTERNAL_SERVER_ERROR,
+    CREATED,
+    NO_CONTENT
+
+}
