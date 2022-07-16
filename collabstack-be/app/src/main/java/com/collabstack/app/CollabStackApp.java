0147635d5d68f6b805badae0a72af478e9e8ed52
devdynam0507 <wsnam0507@naver.com>
2022-07-17 00:08:08 +0900

    메인 모듈 세팅

--- collabstack-be/app/src/main/java/com/collabstack/app/CollabStackApp.java
+++ collabstack-be/app/src/main/java/com/collabstack/app/CollabStackApp.java
@@ -1,9 +1,11 @@
-package net.collabstack.common;
+package com.collabstack.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.collabstack")
 public class CollabStackApp {
 
     public static void main(final String[] args) {
