e0722d115dc1d4f845b9fc82ea43f21527799bb5
devdynam0507 <wsnam0507@naver.com>
2022-07-17 22:01:15 +0900

    Client Configuration 추가

--- collabstack-be/common/src/main/java/net/collabstack/common/client/ClientConfiguration.java
+++ collabstack-be/common/src/main/java/net/collabstack/common/client/ClientConfiguration.java
@@ -1,2 +1,18 @@
-package net.collabstack.common.client;public class ClientConfiguration {
+package net.collabstack.common.client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ClientConfiguration {
+
+    @Bean
+    RestTemplate restTemplate() {
+        final HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(3000); // 타임아웃 설정 5초
+        factory.setReadTimeout(3000); // 타임아웃 설정 5초
+        return new RestTemplate(factory);
+    }
 }
