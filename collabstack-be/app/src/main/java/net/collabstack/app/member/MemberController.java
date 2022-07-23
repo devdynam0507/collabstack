e856611468b76c6654c5d281f2148775b6933e9f
devdynam0507 <wsnam0507@naver.com>
2022-07-24 03:37:31 +0900

    CommonResponse로 변경

--- collabstack-be/app/src/main/java/net/collabstack/app/member/MemberController.java
+++ collabstack-be/app/src/main/java/net/collabstack/app/member/MemberController.java
@@ -1,12 +1,16 @@
 package net.collabstack.app.member;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.collabstack.app.member.domain.Member;
 import net.collabstack.app.member.dto.MemberResponse;
+import net.collabstack.common.CommonResponse;
+import net.collabstack.common.ResultCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,8 +22,9 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/{email}")
-    public MemberResponse getMember(@PathVariable("email") final String email) {
+    public CommonResponse<MemberResponse> getMember(@PathVariable("email") final String email) {
         final Member member = memberService.getMember(email);
-        return MemberResponse.from(member);
+        final MemberResponse memberResponse = MemberResponse.from(member);
+        return CommonResponse.success(ResultCode.CREATED, "멤버를 성공적으로 조회하였습니다.", memberResponse);
     }
 }
